fbbc6adc366a0f1ba223fea33a6ab87f52b33b6f
Jason Hirata <jhirata@integrity-apps.com>
2017-09-30 17:34:18 -1000

    initial fail draw card implement

--- src/index.erl
+++ src/index.erl
@@ -26,15 +26,54 @@ createSide(observer) ->
    ].
 
 createState(auto) ->
-    [{ player1,
-       createSide(auto) },
-     { player2, 
-       createSide(auto) }];
+    [ { phase, start },
+      { turn, player1 },
+      { player1,
+	createSide(auto) },
+      { player2, 
+	createSide(auto) }];
 createState(observer) ->
-    [{ player1,
-       createSide(observer) },
-     { player2,
-       createSide(observer) } ].
+    [ { phase, start },
+      { player1,
+	createSide(observer) },
+      { player2,
+	createSide(observer) } ].
+
+alterState(State, Alter) ->
+    lists:map(Alter,
+	      State).
+
+drawPhase(State = [ Phase, { turn, CurrentPlayer }, { player1, Field1 }, { player2, Field2 } ]) ->
+    DrawCard = fun ({Key, Value}) ->
+		       case Key of
+			   deck ->
+			       [DrawnCard | Deck ] = Value,
+			       { DrawnCard, {deck, Deck} };
+			   _ ->
+			       {_, {Key, Value} }
+		       end
+	       end,
+    FindPlayer = fun({Key, Value}) ->
+			 case Key of 
+			     CurrentPlayer ->
+				 { Context, NewState } = alterState(Value, DrawCard),
+				 { Context, {Key, NewState} };
+			     _ -> 
+				 {_, {Key, Value} }
+			 end
+		 end,
+    alterHand(State, FindPlayer).
+		       
+					    
+		      
+
+loop(State = [{phase,Phase}, { player1, Field1 }, { player2, Field2 } ]) ->
+    case Phase of
+	start ->
+	    loop(drawPhase(State));
+	finish ->
+	    {ok,finish}
+    end.
 
 init() ->
-    io:format("~s~n", ["Hello"]).
+    loop(createState(observer)).
